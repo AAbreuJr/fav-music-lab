c5a81ffa21c667f37261b24bb6af016c929ab267
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-09-17 14:10:48 -0400

    app css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
       <nav>
         <div className="nav-wrapper">
-          <a className=" left" href="/"><img src="https://fontmeme.com/permalink/200705/e2371cf438042048aa22fb9341eadbba.png" height='66' alt="netflix-font" border="0"/></a>
+          <a className=" left" href="/"><img src="https://fontmeme.com/permalink/200917/799698dbca007637a1a64da2ff23e26a.png" height='40' alt="music" border="0"/></a>
           <ul id="nav-mobile" className="right">
             <li><a href="/" className="nav-link">Welcome, {user.name}</a></li>
             <li><a className="nav-link-a" href="/albums">All Albums</a></li>
