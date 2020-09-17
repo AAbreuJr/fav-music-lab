9861ce99ae397cc5781924a02bc4913fcf5975b8
Tyler Reid <tylerreid1988@gmail.com>
2020-09-17 10:57:24 -0400

    add create and export

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,5 +1,13 @@
 const Album = require('../models/album')
 
 module.exports = {
-    
+    create,
+}
+
+function create(req,res) {
+    req.body.addedBy = req.user._id
+    req.body.artist = req.body.artist.split(',');
+    Album.create(req.body)
+    .then(album => {res.json(album)})
+    .catch(err => {res.json(err)})
 }
