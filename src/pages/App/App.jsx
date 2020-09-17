8c7e7e199e04404df34b6caf421224a93a2fd925
Tyler Reid <tylerreid1988@gmail.com>
2020-09-17 11:32:47 -0400

    add app stuff

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,6 +6,7 @@ import Login from "../Login/Login";
 import authService from "../../services/authService";
 import Users from "../Users/Users";
 import "./App.css";
+import * as albumAPI from '../../services/albums-api'
 
 class App extends Component {
   state = {
@@ -22,20 +23,55 @@ class App extends Component {
     this.setState({ user: authService.getUser() });
   };
 
+
+  handleAddAlbum = async newAlbumData => {
+    const newAlbum = await albumAPI.create(newAlbumData);
+    newAlbum.addedBy = {name: this.state.user.name, _id: this.state.user._id }
+    this.setState(state => ({
+      albums: [...state.albums, newAlbum]
+    }), () => this.props.history.push('/albums')) 
+  }
+  
+  handleUpdateAlbum = async updatedAlbumData => {
+    const updatedAlbum = await albumAPI.update(updatedAlbumData);
+    const newAlbumsArray = this.setState.albums.map(a => 
+      a._id === updatedAlbum._id ? updatedAlbum : a
+      );
+      this.setState(
+        {albums: newAlbumsArray}, 
+        () => this.props.history.push('/albums')
+      ) 
+  }
+
+  handleDeleteAlbum = async id => {
+    if(authService.getUser()) {
+      await albumAPI.deleteOne(id);
+      this.setState(state => ({
+        albums: state.albums.filter(a => a._id !== id)
+      }), () => this.props.history.push('/albums'))
+    } else {
+      this.props.history.push('/login')
+    }
+  }
+
+  async componentDidMount() {
+    const albums =await albumAPI.getAll()
+    this.setState({albums})
+  }
+
   render() {
     const {user} = this.state
     return (
       <>
-        <NavBar user={user} handleLogout={this.handleLogout} />
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <main>
-              <h1>Welcome. This is an authorization template.</h1>
-            </main>
-          )}
-        />
+        <NavBar
+         user={this.state.user}
+          handleLogout={this.handleLogout} 
+          />
+
+        <Route exact path='/' render={() =>
+          <Landing />
+        } />
+
         <Route
           exact
           path="/signup"
@@ -46,6 +82,7 @@ class App extends Component {
             />
           )}
         />
+
         <Route
           exact
           path="/login"
@@ -56,6 +93,41 @@ class App extends Component {
             />
           )}
         />
+        
+        <Route
+          exact path='/albums/add'
+          render={() =>
+            authService.getUser() ?
+              <AddAlbum
+                handleAddAlbum={this.handleAddAlbum}
+                user={this.state.user}
+              />
+              :
+              <Redirect to='/login' />
+          } />
+
+        <Route
+          exact path="/albums" render={() =>
+            <AlbumList
+              albums={this.state.albums}
+              user={this.state.user}
+              handleDeleteMovie={this.handleDeleteAlbums}
+            />}
+        />
+
+        <Route
+          exact path='/edit' render={({ location }) =>
+            authService.getUser() ?
+              <EditAlbum
+                handleUpdateAlbum={this.handleUpdateAlbum}
+                location={location}
+                user={this.state.user}
+              />
+              :
+              <Redirect to='/login' />
+          } />
+        
+
         <Route
           exact
           path="/users"
