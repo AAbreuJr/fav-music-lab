5be3b8007e2f3cc64f9f6becc9b9faaac634989f
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-09-17 15:40:25 -0400

    link albums to start

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,9 +6,9 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
       <nav>
         <div className="nav-wrapper">
-          <a className=" left" id="datMusic" href="/"><img src="https://fontmeme.com/permalink/200917/0f9ab0c238a884e7b28670669827382b.png" class="animate__flash" height='40' alt="music" border="0"/></a>
+          <a className=" left" id="datMusic" href="/albums"><img src="https://fontmeme.com/permalink/200917/0f9ab0c238a884e7b28670669827382b.png" class="animate__flash" height='40' alt="music" border="0"/></a>
           <ul id="nav-mobile" className="right">
-            <li><a href="/" className="nav-link">Welcome, {user.name}</a></li>
+            <li><a href="/albums" className="nav-link">Welcome, {user.name}</a></li>
             <li><a className="nav-link-a" href="/albums">All Albums</a></li>
             <li><a className="nav-link-b" href="/albums/add">Add an Album</a></li>
             <li><a className="nav-link-a" href="/songs">All Songs</a></li>
